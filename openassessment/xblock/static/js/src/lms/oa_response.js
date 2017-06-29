f3cb3329caa7bbca423c0dbdd36697c652f490a6
Brandon DeRosier <x@bdero.me>
2017-06-28 21:33:53 -0400

    Added LaTeX preview feature to all comment/feedback fields
    (cherry picked from commit 8fb5af0e3ce7d781a354535e99e3e4c82219e3f4)

--- openassessment/xblock/static/js/src/lms/oa_response.js
+++ openassessment/xblock/static/js/src/lms/oa_response.js
@@ -77,8 +77,10 @@ OpenAssessment.ResponseView.prototype = {
 
         var handlePrepareUpload = function(eventData) { view.prepareUpload(eventData.target.files, uploadType); };
         sel.find('input[type=file]').on('change', handlePrepareUpload);
-        // keep the preview as display none at first
-        sel.find('#submission__preview__item').hide();
+
+        var submit = $('.step--response__submit', this.element);
+        this.textResponse = $(submit).attr('text_response');
+        this.fileUploadResponse = $(submit).attr('file_upload_response');
 
         // Install a click handler for submission
         sel.find('#step--response__submit').click(
@@ -99,19 +101,7 @@ OpenAssessment.ResponseView.prototype = {
         );
 
         // Install click handler for the preview button
-        sel.find('#submission__preview').click(
-            function(eventObject) {
-                eventObject.preventDefault();
-                // extract typed-in response and replace newline with br
-                var previewText = sel.find('.submission__answer__part__text__value').val();
-                var previewContainer = sel.find('#preview_content');
-                previewContainer.html(previewText.replace(/\r\n|\r|\n/g,"<br />"));
-
-                // Render in mathjax
-                sel.find('#submission__preview__item').show();
-                MathJax.Hub.Queue(['Typeset', MathJax.Hub, previewContainer[0]]);
-            }
-        );
+        this.baseView.bindLatexPreview(sel);
 
         // Install a click handler for the save button
         sel.find('#file__upload').click(
