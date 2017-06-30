d104a3f2c453529c2f6793229920b418519d2668
Jillian Vogel <jill@opencraft.com>
2017-06-30 15:24:06 +0930

    Adds django storage backend for ORA2 file uploads. (cherry picked from commit 28ae4e44f92f56ecb9f1cabf1235ee0521b8ad57)

--- openassessment/fileupload/backends/base.py
+++ openassessment/fileupload/backends/base.py
@@ -110,7 +110,6 @@ class BaseBackend(object):
             raise FileUploadRequestError("Key required for URL request")
         return Settings.get_bucket_name(), self._get_key_name(key)
 
-
     def _get_key_name(self, key):
         """Construct a key name with the given string and configured prefix.
 
@@ -127,4 +126,3 @@ class BaseBackend(object):
             prefix=Settings.get_prefix(),
             key=key
         )
-
